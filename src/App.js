d8883f2f78066fa36e1a8f82cbac89a4f2386000
LongLostHero <benjaminsalkic@gmail.com>
2022-04-09 17:01:05 +0200

    HomeScreen
    Eo moj najjaki home screen
    "nema bolje znam da nema" -Jala Brat

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Login from "./screens/login";
-import AdminDashboard from "./screens/admin/dashboard";
+import Home from "./screens/Home";
 
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/admin/dashboard" element={<AdminDashboard />} />
+                <Route path="/home" element={<Home/>} />
             </Routes>
         </BrowserRouter>
     );
