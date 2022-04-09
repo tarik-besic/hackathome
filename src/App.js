86dccf59bbeaba98e214f6afc5990c903dfcf1b5
Amar Silajdzic <90404958+SilaForce@users.noreply.github.com>
2022-04-09 23:03:25 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Login from "./screens/login";
-import AdminDashboard from "./screens/admin/dashboard";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CurrentActions from "./screens/current-actions";
+import SignUp from "./screens/signup";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/admin/dashboard" element={<AdminDashboard />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/projects" element={<CurrentActions />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
