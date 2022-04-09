8d6d496acebfc8aac2e606723a53935f101f08db
LongLostHero <benjaminsalkic@gmail.com>
2022-04-10 01:44:48 +0200

    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 import Home from "./screens/Home";
 import Dashboard from "./screens/dashboard/index";
-import ActiveComp from "./components/ActiveComp";
 
 const App = () => {
     return (
