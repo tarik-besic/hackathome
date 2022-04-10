0fb8153ea2fd98e0d5e55460111779d2ea57cdd6
Amar Silajdzic <90404958+SilaForce@users.noreply.github.com>
2022-04-10 02:22:50 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CurrentActions from "./screens/current-actions";
+import Project from "./screens/project";
 import SignUp from "./screens/signup";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignUp />} />
-        <Route path="/projects" element={<CurrentActions />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/modal" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
