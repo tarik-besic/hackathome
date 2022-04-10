58f56e074ecd21d0b3c7ae64403f4bd1a95de601
LongLostHero <benjaminsalkic@gmail.com>
2022-04-10 05:16:48 +0200

    scss
    asd

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import Home from "./screens/Home";
 import Dashboard from "./screens/dashboard/index";
 import LightBox from "./components/LightBox";
 import CardComp from "./components/CardComp";
+import Trazi from "./screens/Trazi";
 
 const App = () => {
     return (
         <BrowserRouter>
-            <CardComp type="Indirektno" name="Neki Projekt Znas" active={true} desProj="bla blas adasdasdasdas asdasdasdasdasdasd asdasdasd asdasdasdasdasd a adasdas adasdasd adasdad adadadasd adasdasdasdasd adasdslasdl asdlasldak lasedkmc" date="22.4.2022" time="14:00-16:00" img={img1}/>
+        <CardComp type="Indirektno" name="Neki Projekt Znas" active={true} desProj="bla blas adasdasdasdas asdasdasdasdasdasd asdasdasd asdasdasdasdasd a adasdas adk lasedkmc" date="22.4.2022" time="14:00-16:00" img={img1}/>
             <Routes>
                 <Route path="/home" element={<Home/>} />
                 <Route path="/" element={<Dashboard />} />
+                <Route path="/search" element={<Trazi/>} />
             </Routes>
         </BrowserRouter>
     );
--- src/screens/Trazi/index.js
+++ src/screens/Trazi/index.js
@@ -4,16 +4,33 @@ const Trazi = () => {
   return (
     <>
         <div className='search'>
-            <input type="text" placeholder='Naziv' />
-            <input type="text" placeholder='Trajanje'/>
-            <select name="" id="">
+            <div className='top'>
+            <label htmlFor="name">Naziv:</label>
+            <input type="text" name='name' placeholder='Naziv'/>
+            <label htmlFor="trajanje">Trajanje:</label>
+            <input type="text" name='trajanje' placeholder='Trajanje'/>
+            </div>
+            <div className='bot'>
+            <label htmlFor="DKU">Vrsta DKU-a:</label>
+            <select name="DKU" id="">
                 <option value="">Direktni</option>
                 <option value="">Indirektni</option>
                 <option value="">Istrazivanje</option>
                 <option value="">Zagovaranje</option>
             </select>
+            <label htmlFor="Active">Stanje:</label>
+            <select name="Active" id="">
+                <option value="">Aktivni</option>
+                <option value="">Zavrseni</option>
+            </select>
+            </div>
+            <div className='dugme'>
+            <button className='button'>Traži</button>
+            </div>
         </div>
-        <div className='kartice'></div>
+        <div className='kartice'>
+            
+        </div>
     </>
     )
 }
